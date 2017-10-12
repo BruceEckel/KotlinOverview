d303fdf61c76bf29320e8f6e47b470c75a76ec4c
Bruce Eckel <mindviewinc@gmail.com>
2017-10-12 11:12:53 -0600

    Fixed the line-spacing problem

--- postprocess.py
+++ postprocess.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     for line in html.read_text().splitlines():
         result.append(line)
         if line.startswith("td.lineNumbers"):
-            result.append(".reveal pre code { font-size: 1.3em; }")
+            result.append(".reveal pre code { font-size: 1.3em; line-height: 110%; }")
     html.write_text("\n".join(result))
